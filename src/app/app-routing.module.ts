7ac1d2b8aaf074d8a18e9d87e448a1166620546c
OmAiR1Kh <abd.alkhahhar@gmail.com>
2024-02-23 14:14:39 +0200

    table

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UsertableComponent } from './components/usertable/usertable.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "user",
+    component: UsertableComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { UsertableComponent } from './components/usertable/usertable.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    UsertableComponent
   ],
   imports: [
     BrowserModule,
